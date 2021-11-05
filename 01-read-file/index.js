d5403f1127490912bceca260c8d3bb64af9ef5c4
HulionGH <julia.gramovich@mail.ru>
2021-11-05 16:10:53 +0300

    init: start html-builder task

---
+++ 01-read-file/index.js
@@ -0,0 +1,18 @@
+const path = require("path");
+let filePath = path.join(__dirname, "text.txt");
+
+const fs = require("fs");
+const readableStream = fs.createReadStream(filePath, "utf-8");
+let data = "";
+readableStream.on("data", (chunk) => (data += chunk));
+readableStream.on("end", () => console.log(data));
+readableStream.on("error", (error) => console.log("Error", error.message));
+
+/*
+const fs = require("fs");
+const readableStream = fs.createReadStream("text.txt", "utf-8");
+let data = "";
+readableStream.on("data", (chunk) => (data += chunk));
+readableStream.on("end", () => console.log(data));
+readableStream.on("error", (error) => console.log("Error", error.message));
+*/

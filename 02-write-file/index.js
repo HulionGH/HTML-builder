425c2ec36069a984075d8289230820d463fafa3b
HulionGH <julia.gramovich@mail.ru>
2021-11-10 18:42:14 +0300

    fix: fix bug in the read-task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,27 +1,39 @@
 const path = require("path");
 const fs = require("fs");
-const { stdin, stdout } = process;
+const readline = require("readline");
+const { stdin: input, stdout: output } = require("process");
+const rl = readline.createInterface({ input, output });
 
 fs.writeFile(path.join(__dirname, "mynotes.txt"), "", (err) => {
   if (err) throw err;
 });
 let filePath = path.join(__dirname, "mynotes.txt");
 
-const output = fs.createWriteStream(filePath);
-stdout.write(
+const writeStream = fs.createWriteStream(filePath);
+output.write(
   "Введите, пожалуйста, текст в консоль и нажмите Enter для записи! Ctrl+C или exit - чтобы завершить запись\n"
 );
 
-stdin.on("data", (data) => {
+/*stdin.on("data", (data) => {
   const dataStringified = data.toString();
   if (dataStringified.slice(0, -2) === "exit") {
     stdout.write("\nЗапись завершена!");
     process.exit();
   }
-  output.write(dataStringified);
+  writeStream.write(dataStringified.replaceAll("exit", ""));
+});*/
+
+rl.on("line", (data) => {
+  const dataStringified = data.toString();
+  if (dataStringified.includes("exit")) {
+    let result = dataStringified.replace(/exit/g, "");
+    output.write("\nЗапись завершена!");
+    process.exit();
+  }
+  writeStream.write(result);
 });
 
-process.on("SIGINT", () => {
-  stdout.write("\nЗапись завершена!");
+rl.on("SIGINT", () => {
+  output.write("\nЗапись завершена!");
   process.exit();
 });

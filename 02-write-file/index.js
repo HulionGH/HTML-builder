42abc40add951650acfc6771242171ac525185ff
HulionGH <julia.gramovich@mail.ru>
2021-11-10 19:45:37 +0300

    fix: return the first code 3

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,27 +1,30 @@
 const path = require("path");
 const fs = require("fs");
 const { stdin, stdout } = process;
+const readline = require("readline");
+const { stdin: input, stdout: output } = require("process");
+const rl = readline.createInterface({ input, output });
 
 fs.writeFile(path.join(__dirname, "mynotes.txt"), "", (err) => {
   if (err) throw err;
 });
 let filePath = path.join(__dirname, "mynotes.txt");
 
-const output = fs.createWriteStream(filePath);
+const writeStream = fs.createWriteStream(filePath);
 stdout.write(
   "Введите, пожалуйста, текст в консоль и нажмите Enter для записи! Ctrl+C или exit - чтобы завершить запись\n"
 );
 
-stdin.on("data", (data) => {
+rl.on("line", (data) => {
   const dataStringified = data.toString();
   if (dataStringified.slice(0, -2) === "exit") {
     stdout.write("\nЗапись завершена!");
     process.exit();
   }
-  output.write(dataStringified);
+  writeStream.write(dataStringified);
 });
 
-process.on("SIGINT", () => {
+rl.on("SIGINT", () => {
   stdout.write("\nЗапись завершена!");
   process.exit();
 });
